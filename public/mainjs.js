fba31ad31545b4706e37cd876abf043523b36dac
Mazen Jomaa <jommaz@hotmail.com>
2015-04-23 18:00:41 -0400

    Added a navigation bar & footer.

---
+++ public/mainjs.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('#header_nav').data('size','big');
+});
+
+$(window).scroll(function(){
+    var $nav = $('#header_nav');
+    if ($('body').scrollTop() > 0) {
+        if ($nav.data('size') == 'big') {
+            $nav.data('size','small').stop().animate({
+                height:'40px'
+            }, 350);
+        }
+    } else {
+        if ($nav.data('size') == 'small') {
+            $nav.data('size','big').stop().animate({
+                height:'100px'
+            }, 600);
+        }  
+    }
+});
